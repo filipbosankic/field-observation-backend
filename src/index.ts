57e716d1137b4a6fd0fc0bdac1cd31b4a7267843
filip-bosankic <48357512+filipbosankic@users.noreply.github.com>
2026-01-23 01:49:28 +0100

    chore(backend): initial clean TypeScript setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// src/index.ts
+console.log("Backend gestartet");
+
+//import express from "express";
+
+//const app = express();
+//app.get("/health", (_, res) => res.json({ status: "ok" }));
+//
+//app.listen(3000, () => {
+//  console.log("Backend läuft auf Port 3000");
+//});
